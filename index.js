d2ececa8d67764c6a6040db9355690dbf8c64104
CharlieRojas <crc_185@yahoo.com>
2022-03-16 11:33:25 -0500

    commit tree 61131e5a7438eba21d34af368381250353ba7bdc

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(arr,string){
+    let istring = string.toLowerCase();
+    const result = arr.filter(names => names.toLowerCase() == istring);
+    return result
+};
+function fuzzyMatch(arr,string){
+    const result = arr.filter((name) => name.startsWith(string));
+    return result;
+};
+function matchName(driver,string){
+    let istring = result
+    const result = driver.filter(names => names.name == string);
+    return result;
+}
